9482ff83c002fa6849c58fd4719eb1c18225fd2d
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-07-08 00:04:03 -0400

    added other wireframes

--- app/(dashboard)/calendar/page.tsx
+++ app/(dashboard)/calendar/page.tsx
@@ -1,190 +1,226 @@
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-<div className="w-[1324px] h-[823px] relative">
-  <div className="w-[1321px] h-[823px] absolute left-0.5 top-[-1px] bg-[#d9d9d9] border border-black" />
-  <div className="w-[1321px] h-[73px] absolute left-0.5 top-[-1px] bg-[#d9d9d9] border border-black" />
-  <p className="w-[454px] h-[66px] absolute left-[609px] top-[7px] text-5xl text-left text-black">
-    Month
-  </p>
-  <svg
-    width={1}
-    height={750}
-    viewBox="0 0 1 750"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-0.5 top-[72px]"
-    preserveAspectRatio="none"
-  >
-    <line x1="0.5" y1="-2.15934e-8" x2="0.500032" y2={750} stroke="black" />
-  </svg>
-  <svg
-    width={1}
-    height={750}
-    viewBox="0 0 1 750"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[183px] top-[72px]"
-    preserveAspectRatio="none"
-  >
-    <line x1="0.5" y1="-1.53075e-8" x2="0.500046" y2={750} stroke="black" />
-  </svg>
-  <svg
-    width={1}
-    height={750}
-    viewBox="0 0 1 750"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[355px] top-[72px]"
-    preserveAspectRatio="none"
-  >
-    <line x1="0.5" y1="-1.53075e-8" x2="0.500046" y2={750} stroke="black" />
-  </svg>
-  <svg
-    width={1}
-    height={750}
-    viewBox="0 0 1 750"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[540px] top-[72px]"
-    preserveAspectRatio="none"
-  >
-    <line x1="0.5" y1="-1.52937e-8" x2="0.500046" y2={750} stroke="black" />
-  </svg>
-  <svg
-    width={1}
-    height={750}
-    viewBox="0 0 1 750"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[904px] top-[72px]"
-    preserveAspectRatio="none"
-  >
-    <line x1="0.5" y1="-1.52937e-8" x2="0.500046" y2={750} stroke="black" />
-  </svg>
-  <svg
-    width={1}
-    height={750}
-    viewBox="0 0 1 750"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[1116px] top-[72px]"
-    preserveAspectRatio="none"
-  >
-    <line x1="0.5" y1="-1.52937e-8" x2="0.500046" y2={750} stroke="black" />
-  </svg>
-  <svg
-    width={1}
-    height={750}
-    viewBox="0 0 1 750"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[726px] top-[72px]"
-    preserveAspectRatio="none"
-  >
-    <line x1="0.5" y1="-1.53075e-8" x2="0.500046" y2={750} stroke="black" />
-  </svg>
-  <svg
-    width={1322}
-    height={1}
-    viewBox="0 0 1322 1"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[-1px] top-[197px]"
-    preserveAspectRatio="xMidYMid meet"
-  >
-    <line
-      y1="-0.5"
-      x2={1322}
-      y2="-0.5"
-      transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
-      stroke="black"
-    />
-  </svg>
-  <svg
-    width={1322}
-    height={1}
-    viewBox="0 0 1322 1"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[-1px] top-[72px]"
-    preserveAspectRatio="xMidYMid meet"
-  >
-    <line
-      y1="-0.5"
-      x2={1322}
-      y2="-0.5"
-      transform="matrix(1 -8.74228e-08 -8.74228e-08 -1 0 0.00012207)"
-      stroke="black"
-    />
-  </svg>
-  <svg
-    width={1322}
-    height={1}
-    viewBox="0 0 1322 1"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[-1px] top-[700px]"
-    preserveAspectRatio="xMidYMid meet"
-  >
-    <line
-      y1="-0.5"
-      x2={1322}
-      y2="-0.5"
-      transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
-      stroke="black"
-    />
-  </svg>
-  <svg
-    width={1322}
-    height={1}
-    viewBox="0 0 1322 1"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[-1px] top-[575px]"
-    preserveAspectRatio="xMidYMid meet"
-  >
-    <line
-      y1="-0.5"
-      x2={1322}
-      y2="-0.5"
-      transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
-      stroke="black"
-    />
-  </svg>
-  <svg
-    width={1322}
-    height={1}
-    viewBox="0 0 1322 1"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[-1px] top-[324px]"
-    preserveAspectRatio="xMidYMid meet"
-  >
-    <line
-      y1="-0.5"
-      x2={1322}
-      y2="-0.5"
-      transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
-      stroke="black"
-    />
-  </svg>
-  <svg
-    width={1322}
-    height={1}
-    viewBox="0 0 1322 1"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="absolute left-[-1px] top-[449px]"
-    preserveAspectRatio="xMidYMid meet"
-  >
-    <line
-      y1="-0.5"
-      x2={1322}
-      y2="-0.5"
-      transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
-      stroke="black"
-    />
-  </svg>
-</div>
+    <>
+      <div
+        className="w-full h-[113px] relative"
+        style={{ filter: "drop-shadow(0px 4px 4px rgba(0,0,0,0.25))" }}
+      >
+        <div className="w-full h-[104px] absolute left-[-1px] top-[-1px] bg-[#d9d9d9] border border-black" />
+          <button className="w-[293px] h-[54px] absolute left-[19px] top-[35px] text-[40px] text-left text-black">
+            Vitality Venture
+          </button>
+          <button className="w-[88px] h-[54px] absolute left-[743px] top-[59px] text-[32px] text-left text-black">
+            Home
+          </button>
+          <button className="w-[88px] h-[54px] absolute left-[851px] top-[59px] text-[32px] text-left text-black">
+            Shop
+          </button>
+          <button className="w-[147px] h-[54px] absolute left-[959px] top-[59px] text-[32px] text-left text-black">
+            Character
+          </button>
+          <button className="w-[147px] h-[54px] absolute left-[1126px] top-[59px] text-[32px] text-left text-black">
+            Calendar
+          </button>
+
+          <div
+            className="absolute left-[1302px] top-[11px]"
+          >
+            <UserButton
+              appearance={{
+                elements: {
+                    avatarBox: "h-[80px] w-[80px]"
+                }
+              }}
+            />
+          </div>
+      </div>
+      <div className="w-[1324px] h-[823px] relative">
+        <div className="w-[1321px] h-[823px] absolute left-0.5 top-[-1px] bg-[#d9d9d9] border border-black" />
+        <div className="w-[1321px] h-[73px] absolute left-0.5 top-[-1px] bg-[#d9d9d9] border border-black" />
+        <p className="w-[454px] h-[66px] absolute left-[609px] top-[7px] text-5xl text-left text-black">
+          Month
+        </p>
+        <svg
+          width={1}
+          height={750}
+          viewBox="0 0 1 750"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-0.5 top-[72px]"
+          preserveAspectRatio="none"
+        >
+          <line x1="0.5" y1="-2.15934e-8" x2="0.500032" y2={750} stroke="black" />
+        </svg>
+        <svg
+          width={1}
+          height={750}
+          viewBox="0 0 1 750"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[183px] top-[72px]"
+          preserveAspectRatio="none"
+        >
+          <line x1="0.5" y1="-1.53075e-8" x2="0.500046" y2={750} stroke="black" />
+        </svg>
+        <svg
+          width={1}
+          height={750}
+          viewBox="0 0 1 750"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[355px] top-[72px]"
+          preserveAspectRatio="none"
+        >
+          <line x1="0.5" y1="-1.53075e-8" x2="0.500046" y2={750} stroke="black" />
+        </svg>
+        <svg
+          width={1}
+          height={750}
+          viewBox="0 0 1 750"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[540px] top-[72px]"
+          preserveAspectRatio="none"
+        >
+          <line x1="0.5" y1="-1.52937e-8" x2="0.500046" y2={750} stroke="black" />
+        </svg>
+        <svg
+          width={1}
+          height={750}
+          viewBox="0 0 1 750"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[904px] top-[72px]"
+          preserveAspectRatio="none"
+        >
+          <line x1="0.5" y1="-1.52937e-8" x2="0.500046" y2={750} stroke="black" />
+        </svg>
+        <svg
+          width={1}
+          height={750}
+          viewBox="0 0 1 750"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[1116px] top-[72px]"
+          preserveAspectRatio="none"
+        >
+          <line x1="0.5" y1="-1.52937e-8" x2="0.500046" y2={750} stroke="black" />
+        </svg>
+        <svg
+          width={1}
+          height={750}
+          viewBox="0 0 1 750"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[726px] top-[72px]"
+          preserveAspectRatio="none"
+        >
+          <line x1="0.5" y1="-1.53075e-8" x2="0.500046" y2={750} stroke="black" />
+        </svg>
+        <svg
+          width={1322}
+          height={1}
+          viewBox="0 0 1322 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[-1px] top-[197px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line
+            y1="-0.5"
+            x2={1322}
+            y2="-0.5"
+            transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
+            stroke="black"
+          />
+        </svg>
+        <svg
+          width={1322}
+          height={1}
+          viewBox="0 0 1322 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[-1px] top-[72px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line
+            y1="-0.5"
+            x2={1322}
+            y2="-0.5"
+            transform="matrix(1 -8.74228e-08 -8.74228e-08 -1 0 0.00012207)"
+            stroke="black"
+          />
+        </svg>
+        <svg
+          width={1322}
+          height={1}
+          viewBox="0 0 1322 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[-1px] top-[700px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line
+            y1="-0.5"
+            x2={1322}
+            y2="-0.5"
+            transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
+            stroke="black"
+          />
+        </svg>
+        <svg
+          width={1322}
+          height={1}
+          viewBox="0 0 1322 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[-1px] top-[575px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line
+            y1="-0.5"
+            x2={1322}
+            y2="-0.5"
+            transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
+            stroke="black"
+          />
+        </svg>
+        <svg
+          width={1322}
+          height={1}
+          viewBox="0 0 1322 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[-1px] top-[324px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line
+            y1="-0.5"
+            x2={1322}
+            y2="-0.5"
+            transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
+            stroke="black"
+          />
+        </svg>
+        <svg
+          width={1322}
+          height={1}
+          viewBox="0 0 1322 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[-1px] top-[449px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line
+            y1="-0.5"
+            x2={1322}
+            y2="-0.5"
+            transform="matrix(1 -1.30407e-07 -5.86067e-08 -1 0 0)"
+            stroke="black"
+          />
+        </svg>
+    </div>
+    </>
   );
 }
--- app/(dashboard)/character/page.tsx
+++ app/(dashboard)/character/page.tsx
@@ -1,7 +1,77 @@
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <div>
-        Character
-    </div>
+    <>
+      <div
+        className="w-full h-[113px] relative"
+        style={{ filter: "drop-shadow(0px 4px 4px rgba(0,0,0,0.25))" }}
+      >
+        <div className="w-full h-[104px] absolute left-[-1px] top-[-1px] bg-[#d9d9d9] border border-black" />
+          <button className="w-[293px] h-[54px] absolute left-[19px] top-[35px] text-[40px] text-left text-black">
+            Vitality Venture
+          </button>
+          <button className="w-[88px] h-[54px] absolute left-[743px] top-[59px] text-[32px] text-left text-black">
+            Home
+          </button>
+          <button className="w-[88px] h-[54px] absolute left-[851px] top-[59px] text-[32px] text-left text-black">
+            Shop
+          </button>
+          <button className="w-[147px] h-[54px] absolute left-[959px] top-[59px] text-[32px] text-left text-black">
+            Character
+          </button>
+          <button className="w-[147px] h-[54px] absolute left-[1126px] top-[59px] text-[32px] text-left text-black">
+            Calendar
+          </button>
+
+          <div
+            className="absolute left-[1302px] top-[11px]"
+          >
+            <UserButton
+              appearance={{
+                elements: {
+                    avatarBox: "h-[80px] w-[80px]"
+                }
+              }}
+            />
+          </div>
+      </div>
+      <svg
+        width={1228}
+        height={730}
+        viewBox="0 0 1228 730"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-[1228px] h-[729px] relative"
+        preserveAspectRatio="none"
+      >
+        <circle cx={78} cy={75} r="74.5" fill="#DBDBDB" stroke="black" />
+        <line x1="27.7409" y1="20.2843" x2="128.991" y2="129.034" stroke="black" />
+        <line x1="123.381" y1="17.1988" x2="27.756" y2="129.699" stroke="black" />
+        <circle cx={75} cy={345} r="74.5" fill="#DBDBDB" stroke="black" />
+        <line x1="24.7409" y1="290.284" x2="125.991" y2="399.034" stroke="black" />
+        <line x1="120.381" y1="287.199" x2="24.756" y2="399.699" stroke="black" />
+        <circle cx={78} cy={75} r="74.5" fill="#DBDBDB" stroke="black" />
+        <line x1="27.7409" y1="20.2843" x2="128.991" y2="129.034" stroke="black" />
+        <line x1="123.381" y1="17.1988" x2="27.756" y2="129.699" stroke="black" />
+        <circle cx={78} cy={632} r="74.5" fill="#DBDBDB" stroke="black" />
+        <line x1="27.7409" y1="577.284" x2="128.991" y2="686.034" stroke="black" />
+        <line x1="123.381" y1="574.199" x2="27.756" y2="686.699" stroke="black" />
+        <circle cx={1153} cy={75} r="74.5" fill="#DBDBDB" stroke="black" />
+        <line x1="1102.74" y1="20.2843" x2="1203.99" y2="129.034" stroke="black" />
+        <line x1="1198.38" y1="17.1988" x2="1102.76" y2="129.699" stroke="black" />
+        <circle cx={1150} cy={345} r="74.5" fill="#DBDBDB" stroke="black" />
+        <line x1="1099.74" y1="290.284" x2="1200.99" y2="399.034" stroke="black" />
+        <line x1="1195.38" y1="287.199" x2="1099.76" y2="399.699" stroke="black" />
+        <circle cx={1153} cy={75} r="74.5" fill="#DBDBDB" stroke="black" />
+        <line x1="1102.74" y1="20.2843" x2="1203.99" y2="129.034" stroke="black" />
+        <line x1="1198.38" y1="17.1988" x2="1102.76" y2="129.699" stroke="black" />
+        <circle cx={1153} cy={632} r="74.5" fill="#DBDBDB" stroke="black" />
+        <line x1="1102.74" y1="577.284" x2="1203.99" y2="686.034" stroke="black" />
+        <line x1="1198.38" y1="574.199" x2="1102.76" y2="686.699" stroke="black" />
+        <rect x="249.5" y="75.5" width={653} height={653} fill="#D9D9D9" stroke="black" />
+        <line x1="249.354" y1="74.6464" x2="903.354" y2="728.646" stroke="black" />
+        <line x1="903.354" y1="75.3536" x2="249.354" y2="729.354" stroke="black" />
+      </svg>
+    </>
   );
 }
