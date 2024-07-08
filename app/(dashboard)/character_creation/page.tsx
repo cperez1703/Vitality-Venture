2f3c3c7a4e18d21269a400c8a299e823ea71bcc1
cperez1703 <98626649+cperez1703@users.noreply.github.com>
2024-07-07 23:41:40 -0400

    calendar wireframe

--- app/(dashboard)/character_creation/page.tsx
+++ app/(dashboard)/character_creation/page.tsx
@@ -1,7 +1,82 @@
 export default function Home() {
   return (
-    <div>
-        Character Creation
-    </div>
+    <>
+      <div className="w-[1273px] h-[174px] relative">
+        <p className="w-[917px] h-[73px] absolute left-0 top-0 text-[40px] text-left text-black">
+          What would you like to name your baby gator?
+        </p>
+        <div className="w-[195px] h-[83px] absolute left-[748px] top-[90px] bg-[#d9d9d9]" />
+        <div className="w-48 h-[92px] absolute left-[1080px] top-[81px] bg-[#d9d9d9]" />
+        <svg
+          width={184}
+          height={75}
+          viewBox="0 0 184 75"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[760px] top-[164px]"
+          preserveAspectRatio="none"
+        >
+        </svg>
+        <svg
+          width={190}
+          height={72}
+          viewBox="0 0 190 72"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[754px] top-[93px]"
+          preserveAspectRatio="none"
+        >
+        </svg>
+        <svg
+          width={199}
+          height={84}
+          viewBox="0 0 199 84"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[1074px] top-[81px]"
+          preserveAspectRatio="none"
+        >
+        </svg>
+        <svg
+          width={173}
+          height={87}
+          viewBox="0 0 173 87"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[1269px] top-[78px]"
+          preserveAspectRatio="none"
+        >
+        </svg>
+      </div>
+      <div className="w-[1310px] h-[616px] relative">
+    <div className="w-[561px] h-[616px] absolute left-[748px] top-[-1px] bg-[#d9d9d9]" />
+    <svg
+      width={561}
+      height={615}
+      viewBox="0 0 561 615"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="absolute left-[749px] top-px"
+      preserveAspectRatio="none"
+    >
+      <line x1="0.369425" y1="0.663111" x2="560.369" y2="614.663" stroke="black" />
+    </svg>
+    <svg
+      width={551}
+      height={604}
+      viewBox="0 0 551 604"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="absolute left-[759px] top-[602px]"
+      preserveAspectRatio="none"
+    >
+    </svg>
+    <div
+      className="w-[566px] h-[138px] absolute left-[-1px] top-[73px] bg-white border border-black"
+      style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+    />
+    <p className="absolute left-[124px] top-[119px] text-[40px] text-left text-black">Enter name</p>
+  </div>
+    </>
   );
 }
